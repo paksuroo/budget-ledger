e070d53fe9d81b7ea4d9ae4434c9b8981ec433d1
paksuroo <paksuroo@gmail.com>
2026-08-14 20:34:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = "budget-ledger-v1";
+const CACHE = "budget-ledger-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("activate", e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener("fetch", e => {
   if (e.request.method !== "GET") return;
   e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(r => {
